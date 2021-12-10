e68845804f1893ef57a39f382de1c3db08fe64bc
leszakal <83680711+leszakal@users.noreply.github.com>
2021-12-09 16:34:49 -0800

    Update install_ansible_ubuntu.py

--- install_ansible_ubuntu.py
+++ install_ansible_ubuntu.py
@@ -7,10 +7,22 @@ def update():
         print(line.decode())
         
 def install_spc():
-    p = subprocess.check_output([['sudo', 'apt', 'install', 'software-properties-common'])
+    p = subprocess.check_output(['sudo', 'apt', 'install', 'software-properties-common'])
     for line in p.splitlines():
         print(line.decode())
                                  
 def add_repo():
-    p
+    p = subprocess.check_output(['sudo', 'add-apt-repository', '--yes', '--update', 'ppa:ansible/ansible'])
+    for line in p.splitlines():
+        print(line.decode())
                                  
+def install_ansible_pkg():
+    def install_spc():
+    p = subprocess.check_output(['sudo', 'apt', 'install', 'ansible'])
+    for line in p.splitlines():
+        print(line.decode())
+
+update()
+install_spc()
+add_repo()
+install_ansible_pkg()
